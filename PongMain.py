e17a268af2810db5bbc530a51ff76cb37260a3f1
Martin Moors <martin.moors@fpz.de>
2019-01-31 21:15:05 +0100

    Canvas added

--- PongMain.py
+++ PongMain.py
@@ -1,1 +1,11 @@
-print("Hello World!");
+import sys, pygame
+
+pygame.init()
+
+size = width, height = 800, 800
+
+screen = pygame.display.set_mode(size)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
