fc3c2ecc16b350fa64b00ed120c19267b54bcf1d
Martin Moors <martin.moors@fpz.de>
2019-02-03 21:10:32 +0100

    Refactorings

--- PongMain.py
+++ PongMain.py
@@ -1,8 +1,8 @@
 import pygame
 
 SPEED = 100
-WHITE = 255, 255, 255
-BLACK = 0, 0, 0
+BALL_COLOR = 255, 255, 255
+BACKGROUND_COLOR = 0, 0, 0
 
 size = width, height = 1000, 800
 
@@ -13,9 +13,9 @@ screen = pygame.display.set_mode(size)
 
 circle = pygame.Surface((20, 20), pygame.SRCALPHA)
 position = [0, 0]
-steps = [1, 6]
+moveDirection = [1, 6]
 
-pygame.draw.circle(circle, WHITE, [10, 10], 10)
+pygame.draw.circle(circle, BALL_COLOR, [10, 10], 10)
 done = False
 while not done:
     for event in pygame.event.get():
@@ -24,13 +24,13 @@ while not done:
 
     clock.tick(SPEED)
 
-    position[0] += steps[0]
-    position[1] += steps[1]
+    position[0] += moveDirection[0]
+    position[1] += moveDirection[1]
 
     if position[0] > 780 or position[0] < 0:
-        steps[0] = -steps[0]
+        moveDirection[0] = -moveDirection[0]
     if position[1] > 980 or position[1] < 0:
-        steps[1] = -steps[1]
+        moveDirection[1] = -moveDirection[1]
 
     screen.fill(BLACK)
     screen.blit(circle, [position[1], position[0]])
