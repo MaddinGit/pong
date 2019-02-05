37cfc8c54cde3558baf69aeed2ac6311734b4e95
Martin Moors <martin.moors@fpz.de>
2019-02-05 22:02:37 +0100

    Paddle increased in size

--- PongMain.py
+++ PongMain.py
@@ -1,8 +1,8 @@
 import pygame
 
 SPEED = 100
-BALL_COLOR = 255, 255, 255
-BACKGROUND_COLOR = 0, 0, 0
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
 
 size = width, height = 1000, 800
 
@@ -15,11 +15,11 @@ circle = pygame.Surface((20, 20), pygame.SRCALPHA)
 position = [0, 0]
 moveDirection = [1, 6]
 
-paddle = pygame.Surface((9, 100), pygame.SRCALPHA)
-rect = pygame.Rect(0,0,80,90)
-pygame.draw.rect(paddle, BALL_COLOR, rect, 10)
+paddle = pygame.Surface((20, 200), pygame.SRCALPHA)
+rect = pygame.Rect(0,0,10,200)
+pygame.draw.rect(paddle, WHITE, rect, 10)
 
-pygame.draw.circle(circle, BALL_COLOR, [10, 10], 10)
+pygame.draw.circle(circle, WHITE, [10, 10], 10)
 done = False
 while not done:
     for event in pygame.event.get():
@@ -36,7 +36,7 @@ while not done:
     if position[1] > 980 or position[1] < 0:
         moveDirection[1] = -moveDirection[1]
 
-    screen.fill(BACKGROUND_COLOR)
+    screen.fill(BLACK)
     screen.blit(circle, [position[1], position[0]])
     screen.blit(paddle, [30, 30])
     pygame.display.flip()
